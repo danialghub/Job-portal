68a6e4b37b5f1e67c34743efab18eb11804eec4f
dlaz <dlaz@gmail.com>
2025-09-14 18:35:35 +0330

    bug fixed

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -4,7 +4,6 @@ import company_icon from "./company_icon.svg";
 import microsoft_logo from "./microsoft_logo.svg";
 import walmart_logo from "./walmart_logo.svg";
 import accenture_logo from "./accenture_logo.png";
-import profile_img from "./profile_img.png";
 import app_main_img from "./app_main_img.png";
 import cross_icon from './cross_icon.svg';
 import location_icon from './location_icon.svg';
